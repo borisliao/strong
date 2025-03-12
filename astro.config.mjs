4a905916a3b70eb7c63774e3640816de3dd3cf9c
borisliao <borisliaos@gmail.com>
2025-03-12 18:30:16 -0400

    Add Tailwind closes #3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import AstroPWA from "@vite-pwa/astro";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://borisliao.github.io",
   base: "strong",
   integrations: [AstroPWA(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
